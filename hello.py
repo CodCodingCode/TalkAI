292fc8434273dfa5553e70aea84d1595e6ff48e6
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 09:30:39 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,14 +3,12 @@ from PIL import Image
 
 st.title("hello")
 
-img1 = Image.open("Images/Image1.jpeg")
 
 st.subheader("what is this")
 
 
 st.write("hello 2")
 st.write("helloplease")
-st.image(img1)
 
 
 
