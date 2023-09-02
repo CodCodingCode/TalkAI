838b10ba0022b1ca3a039945fb0e2798fc7cb427
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 19:15:16 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -52,10 +52,10 @@ with st.container():
 
 with st.container():
      with st.chat_message("ai", avatar= "🤖"):
-     st.write("Hello 👋")
-     st.line_chart(np.random.randn(30, 3))
-     
-     st.chat_input("Say something")
+          st.write("Hello 👋")
+          st.line_chart(np.random.randn(30, 3))
+          
+          st.chat_input("Say something")
 
 
 
