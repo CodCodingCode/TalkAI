77487454603399685d81edbacb6a0b88ac95b1ae
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 09:56:23 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,14 @@ from PIL import Image
 
 st.set_page_config(page_title="TalkAI", page_icon=":globe_with_meridians", layout="wide")
 st.header('st.button')
-
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+with st.container():
+     if st.button('+ What is TalkAI?'):
+          st.write('TalkAI uses top-of-the-line technology to connect everyone across the world.')
+          st.write("Its free translation features language barrier issues much easier to solve")
+          st.write("TalkAI was created to tackle to everlasting issues of accessibility in our countries,")
+          st.write("though language barriers in different areas.")
+     else:
+          st.write('Goodbye')
 
 
 st.subheader("what is this")
