ad003c1ee099ece75803adabaccf6e9c503f3862
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 19:21:13 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -50,11 +50,8 @@ with st.container():
           if st.button("+ Does Verticai support cheating?"):
                st.write("No, Verticai implicitly supports good usage of education by giving everyone access to educational resources.")
 
-with st.chat_message("ai", avatar= "🤖"):
+with st.chat_message("assisstant", avatar= "🤖"):
      st.write("What is up!")
-
-
-
      st.chat_input("Hello")
 
 
