cc4d9eec95a67b1bbd6771fa8361a2abdb683495
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 19:27:57 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -57,14 +57,12 @@ with st.container():
 with st.container():
      left, mid, right = st.columns([1, 1, 1])
      with mid:
-          if st.button("+ Get Started"):
+          with st.form(key='my_form'):
                while I<3:
                     subject = st.text_input("What is your subject?")
-                    I=I+1
                     grade = st.number_input("What is your grade?")
-                    I=I+1
                     country = st.text_input("What is your country?")
-                    I=I+1
+                    st.formb_submit_button("yes")
 
 
 st.header("People from everywhere around the world don't have access to solid education")
