8313a37633d522c34d382d59bb142c72123f3a06
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 13:53:27 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,48 +3,38 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 import requests
-from streamlit_lottie import st_lottie
-
-def func(url):
-     r = get.requests(url)
-     if request != 200:
-          return None
-     return r.json()
-
-var = func("https://lottie.host/9993e8bf-aa90-4495-b5b0-f188e94a55a7/crWAMmvBzR.json")
 
 
-st.set_page_config(page_title="TalkAI", page_icon=":globe_with_meridians", layout="wide")
+st.set_page_config(page_title="Verticai", page_icon=":globe_with_meridians", layout="wide")
 l, r, m = st.columns([1, 1, 1])
 with r: 
-     st.header('TalkAI')
+     st.header('Verticai')
 
 with st.container():
      left, mid, right = st.columns([1, 1, 1])
      with left:
-          if st.button('+ What is TalkAI?'):
-               st.write('TalkAI uses top-of-the-line technology to connect everyone across the world.')
+          if st.button('+ What is Verticai?'):
+               st.write('Verticai uses top-of-the-line technology to connect everyone across the world.')
                st.write("Its free translation features make problems like language barriers much easier to solve.")
-               st.write("TalkAI was created to tackle to everlasting issues of accessibility in our countries,")
+               st.write("Verticai was created to tackle to everlasting issues of accessibility in our countries,")
                st.write("though language barriers in different areas.")
      with mid:
-          if st.button("+ Is TalkAI free?"):
-               st.write("Yes")
+          if st.button("+ Is Verticai free?"):
+               st.write("Yes, Verticai is completely free! However, there will be a paid version")
+               st.write("to enhance the quality of those who seek better education.")
      with right:
-          if st.button("+ Does TalkAI use Streamlit?"):
-               st.write("yes!")
+          if st.button("+ Does Verticai use Streamlit?"):
+               st.write("Yes! Streamlit's extremely compatible website capabilities")
+               st.write("help out with the web creation at Verticai!")
 
-st.header("Over 75% of the world doesnt speak english AT ALL")
+
+st.header("People from everywhere aroun the world don't have access to solid education")
+
 
 with st.chat_message("user"):
-    st.write("Hello 👋")
+    st.write("Verticai's Education AI")
+
 
 # Display a chat input widget.
-st.chat_input("Say something")
-
-
-st_lottie(var, height=300, key = "coding")
-                    
-
-          
-
+st.chat_input("Input your textbook url")
+        
