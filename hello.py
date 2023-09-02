6a63239622e04799c0d4bab04fe8f81a81824e3a
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 16:39:32 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,17 +4,18 @@ from PIL import Image
 import streamlit as st
 import requests
 
-
-st.set_page_config(page_title="Verticai", page_icon=":globe_with_meridians", layout="wide")
+st.set_page_config(page_title="Verticai", page_icon=":School", layout="wide")
 l, r, m = st.columns([1, 1, 1])
 with r: 
-     st.header('Verticai')
+     st.title("Verticai")
+
+st.subheader("An AI mentor by your side!")
 
 with st.container():
      left, mid, right = st.columns([1, 1, 1])
      with left:
           if st.button('+ What is Verticai?'):
-               st.write("Verticai uses top-of-the-line technology to connect everyone across the world. Its free translation features make problems like language barriers much easier to solve. Verticai was created to tackle to everlasting issues of accessibility in our countries, though language barriers in different areas.")
+               st.write("Verticai uses top-of-the-line technology to help students in need with tutoring and mentorship!")
      with mid:
           if st.button("+ Is Verticai free?"):
                st.write("Yes, Verticai is completely free! However, there will be a paid version to enhance the quality of those who seek better education.")
@@ -22,27 +23,31 @@ with st.container():
           if st.button("+ Does Verticai use Streamlit?"):
                st.write("Yes! Streamlit's extremely compatible website capabilities help out with the web creation at Verticai!")
 
-
-
 with st.container():
      left, mid, right = st.columns([1, 1, 1])
      with left:
           if st.button("+ What is Verticai's end goal"):
-               st.write("Verticai wishes to give everyone a free mentor that can help them with their school work. From any question, to any test, Verticai will do its best to support the inequality in education.")
+               st.write("From anywhere in any country, Verticai wishes to give everyone a free mentor that can help them with their school work!")
      with mid:
           if st.button("+ Can Verticai answer any question?"):
-               st.write("So far, Verticai is in training and can only answer a few questions from various Canadian textbooks like Pearson, etc")
+               st.write("So far, Verticai is in training and can only answer a few questions from various Canadian textbooks like Pearson, Nelson, etc.")
      with right:
           if st.button("+ Does Verticai support cheating?"):
-               st.write("No, Verticai implicitly tries to support good usage of education and helps to empower the world by giving everyone access to education and mentoring rescources")
+               st.write("No, Verticai implicitly supports good usage of education by giving everyone access to educational rescources.")
 
 
-st.header("People from everywhere aroun the world don't have access to solid education")
+st.header("People from everywhere around the world don't have access to solid education")
 
 
-with st.chat_message("user"):
-    st.write("Verticai's Education AI")
+# st.image('image path')
+# st.video('video path')
 
 
-# Display a chat input widget.
-x = st.chat_input("Input your textbook URL")
+
+
+
+# Sign up for our early release
+with st.form(key='my_form'):
+     email = st.text_input('Email')
+     name = st.text_input('Name')
+     st.form_submit_button('Sign up')
