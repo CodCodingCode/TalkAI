53a2122894a6a0df5ae21f5c3c2084642361b676
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 10:33:28 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,22 +2,18 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
+
+upload = 
+transcript = 
+
+headers = {
+     "authorization": auth_key
+     "content-type": "application/json"
+
 st.set_page_config(page_title="TalkAI", page_icon=":globe_with_meridians", layout="wide")
 l, r, m = st.columns([1, 1, 1])
 with r: 
      st.header('TalkAI')
-
-
-
-# Insert a chat message container.
-with st.chat_message("user"):
-     st.write("Hello 👋")
-     st.line_chart(np.random.randn(30, 3))
-
-# Display a chat input widget.
-st.chat_input("Say something")
-
-
 
 with st.container():
      left, mid, right = st.columns([1, 1, 1])
@@ -33,5 +29,15 @@ with st.container():
      with right:
           if st.button("+ Does TalkAI use Streamlit?"):
                st.write("yes!")
+
+
+
+# Insert a chat message container.
+with st.chat_message("user"):
+     st.write("Hello 👋")
+     st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("Say something")
           
 
