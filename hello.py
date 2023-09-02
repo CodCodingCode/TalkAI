530d79f229aee6a850f4575861577576c7ea9831
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 09:59:19 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,13 +4,21 @@ from PIL import Image
 st.set_page_config(page_title="TalkAI", page_icon=":globe_with_meridians", layout="wide")
 st.header('st.button')
 with st.container():
-     if st.button('+ What is TalkAI?'):
-          st.write('TalkAI uses top-of-the-line technology to connect everyone across the world.')
-          st.write("Its free translation features language barrier issues much easier to solve")
-          st.write("TalkAI was created to tackle to everlasting issues of accessibility in our countries,")
-          st.write("though language barriers in different areas.")
-     else:
-          st.write('Goodbye')
+     left, mid, right = st.columns(3)
+     with left:
+          if st.button('+ What is TalkAI?'):
+               st.write('TalkAI uses top-of-the-line technology to connect everyone across the world.')
+               st.write("Its free translation features make problems like language barriers much easier to solve.")
+               st.write("TalkAI was created to tackle to everlasting issues of accessibility in our countries,")
+               st.write("though language barriers in different areas.")
+     with mid:
+          if st.button("+ Is TalkAI free?"):
+               st.write("Yes")
+     with right:
+          if st.button("+ Does TalkAI use Streamlit?"):
+               st.write("yes!")
+          
+
 
 
 st.subheader("what is this")
