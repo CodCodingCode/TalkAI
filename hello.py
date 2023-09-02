da9ce4abe9646088b4061022e0f5f53c7cd7e62c
CodCodingCode <142518277+CodCodingCode@users.noreply.github.com>
2023-09-02 09:23:43 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,9 +2,14 @@ import streamlit as st
 
 st.title("hello")
 
+img1 = Image.open("Images/Image1.jpeg")
+
 st.subheader("what is this")
 
 
 st.write("hello 2")
+st.write("helloplease")
+st.image(img1)
+
 
 
